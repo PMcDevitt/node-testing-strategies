ac15c345b8d7bdc17a6ce82e594db64a41d6cd0c
PMcDevitt <pmcde@allstate.com>
2017-06-18 20:57:42 +0100

    first tests

---
+++ test/Coffee.spec.coffee
@@ -0,0 +1,6 @@
+assert = require('assert')
+
+describe "A second feature", ->
+    describe "A second Scenario", ->
+        it "Just works", ->
+            assert true
---
+++ test/First.spec.js
@@ -0,0 +1,9 @@
+const assert = require('assert')
+
+describe('A Feature', () => {
+    describe('A scenario', () => {
+        it('A test', () => {
+            assert(true)
+        })
+    })
+})
